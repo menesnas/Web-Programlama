2950d1a3a1d2e082ec63e51e1058890962eb23f0
Rashad2173 <133019124+Rashad2173@users.noreply.github.com>
2024-11-29 16:04:57 +0300

    loginpage

---
+++ WebApplication7/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            // Admin girişi kontrolü
+            if (model.UserType == "Admin")
+            {
+                if (ModelState.IsValid && model.Admin.Email == "admin@site.com" && model.Admin.Sifre == "adminpassword")
+                {
+                    return RedirectToAction("Index", "Home"); // Admin giriş yaptıysa Home sayfasına yönlendir
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Geçersiz Admin adı veya şifre.");
+                }
+            }
+            // Kullanıcı girişi kontrolü
+            else if (model.UserType == "Kullanici")
+            {
+                if (ModelState.IsValid && model.Kullanici.Email == "kullanici@site.com" && model.Kullanici.Sifre == "userpassword")
+                {
+                    return RedirectToAction("Index", "Home"); // Kullanıcı giriş yaptıysa Home sayfasına yönlendir
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Geçersiz Kullanıcı adı veya şifre.");
+                }
+            }
+
+            return View("Index", model); // Hata varsa tekrar giriş sayfasını döndür
+        }
+    }
+}
---
+++ WebApplication7/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication7.Models
+{
+    public class LoginViewModel
+    {
+        public Admin Admin { get; set; }
+        public Kullanici Kullanici { get; set; }
+        public string UserType { get; set; } // Kullanıcı türü seçimi için
+    }
+}
